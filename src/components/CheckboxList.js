62a044ee905f40790e27e5e7cbcf689967ece3d9
Erin Kim <kimmiekim@users.noreply.github.com>
2018-10-10 20:44:29 -0400

    move state out of component level

--- src/components/CheckboxList.js
+++ src/components/CheckboxList.js
@@ -10,50 +10,51 @@ export default class CheckboxList extends Component {
   constructor(props){
     super(props);
 
-    this.state = {
-      checkedItems: new Map,
-    }
-    this.handleChange = this.handleChange.bind(this);
-  }
-  handleChange(event){
-    const target = event.target;
-    // const value = target.type === 'checked'? target.checked : target.value;
-    const value = target.value;
-    const item = target.name;
-    const linkUrl = target.linkUrl;
-    const imgUrl = target.imgUrl;
-    const isChecked = event.target.checked;
+    // this.state = {
+    //   checkedItems: new Map(),
+    // }
+    // this.handleChange = this.handleChange.bind(this);
 
-    //add to state array using spread operator
-    //next step is to filter newly added value so it's not repeated
-    this.setState( prev => ({
-      checkedItems: prev.checkedItems.set(item, isChecked)
-    }));
-    // console.log("checked");
-    //
-    console.log("names", this.state.checkedItems);
   }
 
+  // handleChange(event){
+  //   const target = event.target;
+  //   // const value = target.type === 'checked'? target.checked : target.value;
+  //   const value = target.value;
+  //   const item = target.name;
+  //   const linkUrl = target.linkUrl;
+  //   const imgUrl = target.imgUrl;
+  //   const isChecked = event.target.checked;
+  //
+  //   //add to state array using spread operator
+  //   //next step is to filter newly added value so it's not repeated
+  //   this.setState( prev => ({
+  //     checkedItems: prev.checkedItems.set(item, isChecked)
+  //   }));
+  //
+  //
+  // }
+  changeCheckeditems = (data) => {
+    this.props.onHandleChange(data);
+    this.props.openQuicklinkBox(data)
+  }
 
   render() {
+    const { checkedItems, checkboxes } = this.props
     return (
-      <div>
-      <React.Fragment>
+      <div className="checkbox-form">
         {
-          Checkboxes.map(data => (
+          checkboxes.map(data => (
             <label key={data.key}>
               {data.name}, {data.linkUrl}
-              <Checkbox name={data.name} checked= {this.state.checkedItems.get(data.name)} onChange={this.handleChange} /><br />
+              <Checkbox name={data.name} checked={checkedItems.get(data.name)} onChange={(data) => this.changeCheckeditems(data)} linkUrl={data.linkUrl} /><br />
+
             </label>
+
           ))
         }
-      </React.Fragment>
-      {/* <React.Fragment>
-        {
-          this.state.checkedItems? Object.keys(this.state.checkedItems).map(item => console.log("item:",item)) :''
-        }
-      </React.Fragment> */}
       </div>
+
 
     );
   }
