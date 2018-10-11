e2c821b0a54f7d46660897a0287cced46d4546df
Erin Kim <kimmiekim@users.noreply.github.com>
2018-10-10 20:43:29 -0400

    updated data

--- src/components/Checkboxes.js
+++ src/components/Checkboxes.js
@@ -11,21 +11,21 @@ const Checkboxes = [
     name: 'check-box-2',
     key: 'checkBox2',
     label: 'Check Box 2',
-    linkUrl: 'checkbox-1.ca',
+    linkUrl: 'checkbox-2.ca',
     imgUrl: 'checkbox-1.jpg'
   },
   {
     name: 'check-box-3',
     key: 'checkBox3',
     label: 'Check Box 3',
-    linkUrl: 'checkbox-1.ca',
+    linkUrl: 'checkbox-3.ca',
     imgUrl: 'checkbox-1.jpg'
   },
   {
     name: 'check-box-4',
     key: 'checkBox4',
     label: 'Check Box 4',
-    linkUrl: 'checkbox-1.ca',
+    linkUrl: 'checkbox-4.ca',
     imgUrl: 'checkbox-1.jpg'
   },
 ];
